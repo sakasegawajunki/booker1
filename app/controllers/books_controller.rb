f4c5c21574ea31430f438643f3bc28d5648802ef
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-05-08 08:52:40 +0000

    [Finish]19

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,12 @@ class BooksController < ApplicationController
     redirect_to book_path(book.id)
     
   end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
 
   def edit
   end
