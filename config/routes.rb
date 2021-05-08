c77d82dbe47ef02873c4f79d1fc52fd90c73908c
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-05-08 06:42:44 +0000

    [Finish]18

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :b
+  resources :books
   
-  get "top" => "homes#top"
+
+  root :to => "homes#top"
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
